0a4826a02ce50d1edefa1e03fc6de5fa3df3e18e
valentinshenfeld <shenfeld.valentin@gmail.com>
2024-04-23 15:27:48 +0300

    add front and scripts files

---
+++ front/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "DApp MusicShope",
+  description: "You can buy and sell your favorite singl for EHT",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
